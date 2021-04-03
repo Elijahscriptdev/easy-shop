8e4ff6c69a4dbc0b1e941c2290c297e9053feb71
Elijahscriptdev <elijahobominuru@gmail.com>
2021-04-04 00:03:14 +0100

    project set-up

---
+++ Screens/Products/ProductContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+const ProductContainer = () => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default ProductContainer
